5e321e9323510615c99293734e0eea8393917782
Andrew Duthie <andrew@andrewduthie.com>
2021-06-08 21:26:40 -0400

    Update rollup

--- rollup.iife.config.js
+++ rollup.iife.config.js
@@ -1,5 +1,5 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default /** @type {import('rollup').RollupOptions} */ ( {
 	input: 'es/rememo.js',

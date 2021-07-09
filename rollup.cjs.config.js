d9f0eb5d09048572d778256d9bd379e39d31b97c
Andrew Duthie <andrew@andrewduthie.com>
2021-07-08 20:08:01 -0400

    Combine rollup configurations

--- rollup.cjs.config.js
+++
@@ -1,8 +0,0 @@
-export default /** @type {import('rollup').RollupOptions} */ ({
-	input: 'es/rememo.js',
-	output: {
-		file: 'rememo.js',
-		format: 'cjs',
-		exports: 'default',
-	},
-});
---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default /** @type {import('rollup').RollupOptions[]} */ [
+	{
+		input: 'es/rememo.js',
+		output: {
+			file: 'rememo.js',
+			format: 'cjs',
+			exports: 'default',
+		},
+	},
+	{
+		input: 'es/rememo.js',
+		output: {
+			file: 'dist/rememo.js',
+			name: 'rememo',
+			exports: 'default',
+			format: 'iife',
+		},
+		plugins: [resolve(), commonjs()],
+	},
+];
